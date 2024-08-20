63943570cb014ed04964d6f4cf60ec211f9a40e8
Dustin Aldana <71577541+D-Aldana@users.noreply.github.com>
2024-08-19 17:53:15 -0700

    Added filler and commented actual functionality for start/stop webcam

--- backend/app.py
+++ backend/app.py
@@ -12,28 +12,29 @@ socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
 #################### FUNCTIONS ####################
 
 def start_all_gopros():
-    success = True
     for ip in gopro_ips:
         print(f'Starting GoPro at {ip}')
-        # TODO - Add code to start GoPro
-        
-    return success
+        ####### USE WHEN READY TO TEST
+        # url = f'http://{ip}:8080/gopro/webcam/start'
+        # response = requests.request("GET", url)
+        # print(response.json())
 
 def stop_all_gopros():
-    success = True
     for ip in gopro_ips:
         print(f'Stopping GoPro at {ip}')
-        # TODO - Add code to stop GoPro
-    return success
+        ####### USE WHEN READY TO TEST
+        # url = f'http://{ip}:8080/gopro/webcam/stop'
+        # response = requests.request("GET", url)
+        # print(response.json())
 
-def get_gopro_status():
+def get_gopro_status(status=0):
     responses = []
     for ip in gopro_ips:
         ####### USE WHEN READY TO TEST
-        # url = f'http://{ip}/gopro/webcam/status'
+        # url = f'http://{ip}:8080/gopro/webcam/status'
         # response = requests.request("GET", url)
         # responses.append({'ip': ip, 'status': response.json()})
-        response = {'error': 0, 'status': 1}
+        response = {'error': 0, 'status': status}
         responses.append({'ip': ip, 'status': response})
     
     return responses
@@ -47,18 +48,14 @@ def refresh_gopro_status():
 @socketio.on('start_gopros')
 def start_gopros():
     print('Starting GoPros')
-    if start_all_gopros():
-        emit('gopro_status', get_gopro_status())
-    else:
-        emit('gopro_status', get_gopro_status())
+    start_all_gopros()
+    emit('gopro_status', get_gopro_status(1))
 
 @socketio.on('stop_gopros')
 def stop_gopros():
     print('Stopping GoPros')
-    if stop_all_gopros():
-        emit('gopro_status', get_gopro_status())
-    else:
-        emit('gopro_status', get_gopro_status())
+    stop_all_gopros()
+    emit('gopro_status', get_gopro_status(0))
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000, debug=True)
